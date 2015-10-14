44ee9f2e0d556d79c48244a841d1e3af842df51d
Andrew McCann <amccann@gmail.com>
2015-10-13 20:00:01 -0700

    make QICons load from shared and app specific themes

--- kf5-kiconthemes.rb
+++ kf5-kiconthemes.rb
@@ -30,25 +30,55 @@ end
 __END__
 
 diff --git a/src/kicontheme.cpp b/src/kicontheme.cpp
-index d0ab4b9..e4c3c61 100644
+index d0ab4b9..535956b 100644
 --- a/src/kicontheme.cpp
 +++ b/src/kicontheme.cpp
-@@ -171,6 +171,20 @@ KIconTheme::KIconTheme(const QString &name, const QString &appName, const QStrin
+@@ -171,6 +171,50 @@ KIconTheme::KIconTheme(const QString &name, const QString &appName, const QStrin
          return;
      }
  
 +#ifdef Q_OS_MAC
++    // make QIcon aware of the theme & paths 
++
 +    // only update search paths once.
 +    auto paths = QIcon::themeSearchPaths();
 +    // assume count()==1 is the default {":/icons"} case
 +    if (paths.count()==1) {
-+        paths += icnlibs;
-+        QIcon::setThemeSearchPaths( paths );
-+        //qWarning() << "QIcon::setThemeSearchPaths(" << paths << ")";
-+    }
 +
-+    //qWarning() << "QIcon::setThemeName(" << name << ")";
-+    QIcon::setThemeName(name);
++        // app overloads first
++        paths += QStandardPaths::locateAll(QStandardPaths::AppDataLocation, "icons", QStandardPaths::LocateDirectory);
++        // generics after
++        paths += icnlibs;
++
++        QIcon::setThemeSearchPaths( paths );
++        qWarning() << "QIcon::setThemeSearchPaths" << paths ;
++
++
++        // warn about 'inherited' themes that will not be seen
++        {
++            auto getWarnings = [&](const QString& str) {
++                QStringList ret;
++                if (name != str) {
++                    ret = QStandardPaths::locateAll(QStandardPaths::AppDataLocation, "icons/" + str, QStandardPaths::LocateDirectory);
++                }
++                return ret;
++            };
++
++            QStringList warning_paths;
++            warning_paths += getWarnings(defaultThemeName());
++            warning_paths += getWarnings(QLatin1String("hicolor"));
++            warning_paths += getWarnings(QLatin1String("locolor"));
++
++            for ( auto&& warning_path : warning_paths ) {
++                qWarning() << "OSX only: Unsupported inheriting icon theme found at '" <<  warning_path << "'"
++                              "\n\tTry renaming or merging to/into '" << name << "'";
++            }
++        }
++    }
++    if (QIcon::themeName() != name) {
++        QIcon::setThemeName(name);
++        qWarning() << "QIcon::themeName(): " << QIcon::themeName();
++    }
 +#endif
 +
      // Use KSharedConfig to avoid parsing the file many times, from each component.
